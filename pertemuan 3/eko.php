26fa7c3705aa31d1595b0e196a9cdeb13efd8cf1
EkoSusanRiyanto <susaneko279@gmail.com>
2022-01-13 16:18:50 +0700

    menambahkan file pertemuan 3

---
+++ pertemuan 3/eko.php
@@ -0,0 +1,20 @@
+<?php
+    $nama = "Eko Susan Riyanto";
+    $NIM = "1904030022";
+    $kelas = "5B";
+    $umur = (0x14);
+    $Alamat = "Wismamas II Pasar Kemis Kab.Tangerang";
+    $Hobi = "Bermain Game";
+    $No_Telp = "088709798180";
+    $Agama="Islam";
+?>
+    
+    <h2>Identitas diri</h2>
+    <p>Nama : <?php echo $nama; ?></p>
+    <p>NIM  : <?php echo $NIM; ?></p>
+    <p>Kelas : <?php echo $kelas; ?></p>
+    <p>Umur : <?php echo $umur; ?></p>
+    <p>Alamat   : <?php echo $Alamat; ?></p>
+    <p>Hobi : <?php echo $Hobi; ?></p>
+    <p>Nomer Telepon : <?php echo $No_Telp; ?></p>
+    <p>Agama    : <?php echo $Agama; ?></p> 
