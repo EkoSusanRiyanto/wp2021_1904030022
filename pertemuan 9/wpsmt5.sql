696468bf468e8f3b4e24c30b9c7428360daed806
EkoSusanRiyanto <susaneko279@gmail.com>
2022-01-13 22:40:58 +0700

    menambahkan file pertemuan 9

---
+++ pertemuan 9/wpsmt5.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Jan 2022 pada 09.29
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wpsmt5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `calon_mhs`
+--
+
+CREATE TABLE `calon_mhs` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(40) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(9) NOT NULL,
+  `agama` varchar(9) NOT NULL,
+  `sekolah_asal` varchar(20) NOT NULL,
+  `foto_maba` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `calon_mhs`
+--
+
+INSERT INTO `calon_mhs` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`, `foto_maba`) VALUES
+(1, 'jono', 'veteran 2', 'laki laki', 'islam', 'sma 12', 'photo1.png'),
+(2, 'joni', 'indah raya 2', 'laki laki', 'islam', 'sma 12', 'photo1.png'),
+(3, 'rival', 'perum 1', 'laki laki', 'islam', 'smk bisa', 'photo1.png'),
+(4, 'doni', 'indah asri 2', 'laki laki', 'islam', 'smk 1 mauk ', 'photo1.png'),
+(5, 'rio', 'gembor', 'laki laki', 'islam', 'sma 11', 'photo2.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `calon_mhs`
+--
+ALTER TABLE `calon_mhs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
